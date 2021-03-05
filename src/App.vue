5193c70867051609dbbf78bbf0ee02ebebe5e8a5
bezedache29 <bezedache29@gmail.com>
2021-03-05 15:50:22 +0100

    Tophe (#2)
    * Création de projet avec vue.js
    
    * Ajout du serviceWorker.js / logo / manifest.json + MAJ de index.html
    
    * Création du header / navbar/ bouton
    
    * Rajout name au  header / navbar/ bouton

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div>
+    <header-app />
+    <home-app />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import HeaderApp from './components/Headers/HeaderApp'
+import HomeApp from './components/Sections/HomeApp'
+
+<script>
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+    HeaderApp,
+    HomeApp
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+/*** RESET ***/
+html {
+    box-sizing: border-box;
+    font-size: 16px;
+    height: 100%;
+}
+*, *:before, *:after {
+    box-sizing: inherit;
+}
+body, h1, h2, h3, h4, h5, h6, p, ol, ul {
+    margin: 0;
+    padding: 0;
+    font-weight: normal;
+}
+ol, ul {
+    list-style: none;
+}
+a {
+    text-decoration: none;
+}
+img {
+    max-width: 100%;
+    height: auto;
+}
+body {
+    height: 100%;
 }
 </style>
