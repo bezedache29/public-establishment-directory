85e6cfb2efd4f0c8a2650623b14a4c0d4fe2693a
bezedache29 <bezedache29@gmail.com>
2021-03-07 00:05:37 +0100

    Tophe (#3)
    * Maj Tophe
    
    * Ajout des pages Home et Search

--- src/components/Sections/HomeApp.vue
+++ src/components/Sections/HomeApp.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-
+    <h1>Annuaire des établissements publics de l'administration</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus vitae incidunt reprehenderit temporibus quasi assumenda? Eligendi, quaerat. Minima animi iure ipsum maiores natus, eos, tempora facilis aperiam nulla molestiae corrupti?
+    Consequatur cum quasi, atque quidem quos laudantium, eaque quia unde consequuntur culpa aut dicta quo, commodi esse. Assumenda placeat iure doloremque odio quos mollitia? Ad ut a officiis reprehenderit accusamus!
+    Id repellat delectus hic ipsam minus nemo temporibus officia dolor qui vitae, iste ducimus soluta provident similique enim sit maiores itaque possimus, corrupti dolores assumenda voluptatum? Repellat, magnam? Ratione, quos.
+    Ut tenetur expedita excepturi atque natus molestiae temporibus animi soluta dignissimos asperiores ab qui odio incidunt, sunt reprehenderit veritatis omnis, deserunt deleniti repellat pariatur saepe aperiam consequuntur dolore? Iure, laborum?
+    Rem, fuga ullam, officiis itaque soluta impedit dolore asperiores ea nostrum unde error. Cupiditate nesciunt fugiat exercitationem consequuntur dolorem, repellendus magni quia, unde labore similique expedita sit, iste eligendi neque?
+    Quisquam, facilis. Repellat, tempore magnam consequuntur sed beatae hic veniam ducimus, molestiae, a nobis aperiam. Sapiente ipsum a, nobis obcaecati dolorum sequi, iste ducimus sed, sint suscipit adipisci officia esse!
+    Ipsam dolore ullam omnis. Quasi, magni obcaecati excepturi deleniti dolorum repellat voluptas aut nisi eius laborum beatae nihil ipsa animi esse placeat vitae veritatis adipisci vero, quo error nam tempore.
+    Similique aperiam hic excepturi doloribus sit ad blanditiis suscipit quisquam harum rerum nesciunt itaque tempore natus, obcaecati porro aliquam maxime optio perferendis placeat nobis quod voluptate, totam quia! Dolore, dolorem.
+    Facere quidem fugit explicabo amet quos eligendi iste iusto et vitae voluptas incidunt unde natus cupiditate maxime reprehenderit, id eaque. Et, nihil. Repellat obcaecati velit, minima molestias iusto hic. Laborum.
+    Minus expedita sit, iure eum dolores assumenda non! Neque, dolorem harum explicabo provident nihil error quisquam quia ab ut dolore porro odit vero quaerat consequatur itaque? Sint nam placeat quas?</p>
   </div>
 </template>
 
