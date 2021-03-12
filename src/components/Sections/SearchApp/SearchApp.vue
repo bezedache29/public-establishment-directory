201aa2aa475d16a0efe86877436a1c3835f3bceb
bezedache29 <bezedache29@gmail.com>
2021-03-12 14:54:34 +0100

    Installation d'Axios pour requête API

--- src/components/Sections/SearchApp/SearchApp.vue
+++ src/components/Sections/SearchApp/SearchApp.vue
@@ -1,17 +1,51 @@
 <template>
-  <h1>Search Page</h1>
+  <section class="search">
+    <h1 class="search__title">Rechercher un établissement</h1>
+
+    <div class="search__container">
+
+      <div class="search__container__form-group">
+        <label class="search__container__form-group--label" for="department">Indiquez le numéro du département</label>
+        <input 
+          class="search__container__form-group--input" 
+          id="department" 
+          type="number" 
+          v-model="department" 
+          placeholder="Ex: 29"
+          @keyup="searchDept"
+        />
+      </div>
+
+
+    </div>
+  </section>
 </template>
 
 <script>
 
-  import './_search-app.scss'
+  import './../../../assets/scss/style.scss'
 
   export default {
-    name:"",
+    name:"search-app",
+
     data() {
       return {
-        
+        department: null,
+        results: {},
+        apiUrl: 'https://etablissements-publics.api.gouv.fr/v3/departements'
       }
     },
+
+    methods: {
+      searchDept() {
+        if (this.department !== null && this.department <= 95) {
+
+          this.axios.get(`${this.apiUrl}/${this.department}/mairie`).then((response) => {
+            console.log(response.data)
+          })
+
+        }
+      }
+    }
   }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.mount('#app')
+app.use(VueAxios, axios)
