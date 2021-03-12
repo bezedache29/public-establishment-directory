0c4fe309e421a81d8a2ff2f6971fe40df0b830c6
bezedache29 <bezedache29@gmail.com>
2021-03-12 16:26:32 +0100

    Ajout des composants card et bouton

---
+++ src/components/Sections/SearchApp/CardsApp/CardApp.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card">
+    <div class="card__title">Card title</div>
+    <div class="card__body">Card Body</div>
+  </div>
+</template>
+
+<script>
+
+import './../../../../assets/scss/style.scss'
+
+export default {
+
+  name:"card-app",
+
+}
+</script>
